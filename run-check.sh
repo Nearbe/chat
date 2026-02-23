d60d52c20a57375863cef9c9701cfd8259ea9463
Гудзенко Евгений Олегович <eogudzenko@corp.finam.ru>
2026-02-23 16:54:20 +0300

    Automatic commit after successful verification

--- run-check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-exec 3>&1
-xcodebuild -scheme "🔍 Check" build > /dev/null 2>&1
-/Users/nearbe/Library/Developer/Xcode/DerivedData/Chat-cpfexcnvwheubjfivmviimoteein/Build/Products/Debug/scripts check >&3
